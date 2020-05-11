edb84215f958df3c48b3394ce9ae87d3334e1361
kiku ishimoto <kikuish@gmail.com>
2020-05-11 19:01:09 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,12 +1,11 @@
 class Person
   def initialize(name)
-    @name = name 
-  end 
-  def name 
-    @name 
-  end 
+    @name = name
+  end
+  def name
+    @name
+  end
   def name=(their_name)
     @name = their_name
-  end 
+  end
 end
-    
